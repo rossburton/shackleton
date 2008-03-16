b6c32d6cd0f50718d5c4fb4e355ecc688233b35f
Ross Burton <ross@burtonini.com>
2008-03-16 17:21:45 +0000

    Add GConf action, and screensaver lock subclass

--- actions.py
+++ actions.py
@@ -16,3 +16,17 @@ class SpawnAction(Action):
     def run(self):
         from subprocess import Popen
         Popen(self.command, shell=True)
+
+class GConfAction(Action):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def run(self):
+        import gconf
+        client = gconf.client_get_default()
+        client.set_value(self.key, self.value)
+
+class ScreensaverLockAction(GConfAction):
+    def __init__(self, lock):
+        GConfAction.__init__(self, "/apps/gnome-screensaver/lock_enabled", lock)
