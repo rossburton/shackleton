85f74e02dc75350759de70fbe8d5a175855be770
Ross Burton <ross@burtonini.com>
2008-03-16 15:19:57 +0000

    Add SpawnAction

--- actions.py
+++ actions.py
@@ -8,3 +8,11 @@ class DebugAction(Action):
 
     def run(self):
         print self.s
+
+class SpawnAction(Action):
+    def __init__(self, command):
+        self.command = command
+
+    def run(self):
+        from subprocess import Popen
+        Popen(self.command, shell=True)
