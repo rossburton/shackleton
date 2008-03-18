39b6f106b14918e80855cfb3156855d71e61b687
Ross Burton <ross@burtonini.com>
2008-03-18 20:16:22 +0000

    Add __str__ methods to the actions

--- actions.py
+++ actions.py
@@ -2,12 +2,14 @@ class Action:
     def run(self):
         pass
 
+
 class DebugAction(Action):
     def __init__(self, s):
         self.s = s
 
     def run(self):
         print self.s
+
 
 class SpawnAction(Action):
     def __init__(self, command):
@@ -16,6 +18,10 @@ class SpawnAction(Action):
     def run(self):
         from subprocess import Popen
         Popen(self.command, shell=True)
+
+    def __str__(self):
+        return "Running %s" % self.command
+
 
 class GConfAction(Action):
     def __init__(self, key, value):
@@ -27,6 +33,16 @@ class GConfAction(Action):
         client = gconf.client_get_default()
         client.set_value(self.key, self.value)
 
+    def __str__(self):
+        return "Setting GConf key %s", self.key
+
+
 class ScreensaverLockAction(GConfAction):
     def __init__(self, lock):
         GConfAction.__init__(self, "/apps/gnome-screensaver/lock_enabled", lock)
+
+    def __str__(self):
+        if self.value:
+            return "Enabling screensaver lock"
+        else:
+            return "Disabling screensaver lock"
