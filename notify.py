c8c269a6ab65c7872bae84e0e3e3447173c452a5
Ross Burton <ross@burtonini.com>
2008-03-20 10:24:24 +0000

    Don't show a colon in the notify in case there are no actions

--- notify.py
+++ notify.py
@@ -19,13 +19,13 @@ import pynotify
 pynotify.init("Shackleton")
 
 def enter(context):
-    l = ["<b>Entering %s context:</b>" % context]
+    l = ["<b>Entering %s context</b>" % context]
     l += [str(a) for a in context.entering_actions]
     details = "\n\342\200\242 ".join(l)
     pynotify.Notification("Changing Context", details).show()
 
 def leave(context):
-    l = ["<b>Leaving %s context:</b>" % context]
+    l = ["<b>Leaving %s context</b>" % context]
     l += [str(a) for a in context.leaving_actions]
     details = "\n\342\200\242 ".join(l)
     pynotify.Notification("Changing Context", details).show()
